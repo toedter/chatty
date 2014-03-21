f5d3ef1776337a4b5bb81c78a7ecd83fd9414a4d
Kai Tödter <kai@toedter.com>
2014-03-21 16:25:07 +0100

    added more copyright info

--- subprojects/com.toedter.chatty.server/src/integTest/java/com/toedter/chatty/server/resources/JettyIntegrationTest.java
+++ subprojects/com.toedter.chatty.server/src/integTest/java/com/toedter/chatty/server/resources/JettyIntegrationTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2014 Kai Toedter
+ * All rights reserved.
+ * Licensed under MIT License, see http://toedter.mit-license.org/
+ */
+
 package com.toedter.chatty.server.resources;
 
 import org.atmosphere.cpr.AtmosphereServlet;
