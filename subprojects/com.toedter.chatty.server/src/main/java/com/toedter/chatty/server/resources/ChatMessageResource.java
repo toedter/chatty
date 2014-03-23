7f8370334b4dc23ea7d3830d6e0b4f1cf3486487
Kai Toedter <kai@toedter.com>
2014-03-23 20:14:46 +0100

    better HAL representation of chat messages

--- subprojects/com.toedter.chatty.server/src/main/java/com/toedter/chatty/server/resources/ChatMessageResource.java
+++ subprojects/com.toedter.chatty.server/src/main/java/com/toedter/chatty/server/resources/ChatMessageResource.java
@@ -12,7 +12,6 @@ import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
 import com.toedter.chatty.model.ChatMessage;
 import com.toedter.chatty.model.ChatMessageRepository;
 import com.toedter.chatty.model.ModelFactory;
-import com.toedter.chatty.model.User;
 import org.atmosphere.client.TrackMessageSizeInterceptor;
 import org.atmosphere.config.service.AtmosphereService;
 import org.atmosphere.cpr.BroadcasterFactory;
@@ -20,10 +19,7 @@ import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
@@ -42,21 +38,52 @@ public class ChatMessageResource {
 
     @GET
     @Produces(RepresentationFactory.HAL_JSON)
-    public String getUsers(@Context UriInfo uriInfo) {
+    public String getChatMessages(@Context UriInfo uriInfo) {
         String baseURI = uriInfo.getRequestUri().toString();
 
         Representation listRep = representationFactory.newRepresentation();
         listRep.withLink("self", baseURI, null, null, "en", "chatty");
 
         ChatMessageRepository chatMessageRepository = ModelFactory.getInstance().getChatMessageRepository();
+        String authorBaseURI = baseURI.replace("messages", "users");
 
         for (ChatMessage chatMessage : chatMessageRepository.getAll()) {
             Representation rep = representationFactory.newRepresentation();
-            rep.withBean(chatMessage)
-                    .withLink("self", baseURI + "/" + chatMessage.getId());
+            rep.withLink("self", baseURI + "/" + chatMessage.getId())
+                    .withLink("author", authorBaseURI + "/" + chatMessage.getAuthor().getId());
+
+//            Representation authorRep = representationFactory.newRepresentation();
+//            authorRep.withBean(chatMessage.getAuthor());
+//            rep.withRepresentation("author", authorRep);
+
+            rep.withProperty("id", chatMessage.getId())
+                    .withProperty("message", chatMessage.getMessage())
+                    .withProperty("timeStamp", chatMessage.getTimeStamp().toString());
+
             listRep.withRepresentation("messages", rep);
         }
         return listRep.toString(RepresentationFactory.HAL_JSON);
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(RepresentationFactory.HAL_JSON)
+    public String getChatMessage(@Context UriInfo uriInfo, @PathParam("id") final long id) {
+        Representation rep = representationFactory.newRepresentation();
+        String baseURI = uriInfo.getRequestUri().toString();
+
+        ChatMessage chatMessage = ModelFactory.getInstance().getChatMessageRepository().getChatMessageById(id);
+        rep.withLink("self", baseURI);
+
+        Representation authorRep = representationFactory.newRepresentation();
+        authorRep.withBean(chatMessage.getAuthor());
+        rep.withRepresentation("author", authorRep);
+
+        rep.withProperty("id", chatMessage.getId())
+                .withProperty("message", chatMessage.getMessage())
+                .withProperty("timeStamp", chatMessage.getTimeStamp().toString());
+
+        return rep.toString(RepresentationFactory.HAL_JSON);
     }
 
     @POST
