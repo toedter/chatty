959cce0d6de5671a409e6b3355768f49712bd5e5
Kai Tödter <kai@toedter.com>
2014-05-30 14:35:41 +0200

    fixed functional test (with protractor)

--- subprojects/com.toedter.chatty.client.web/src/funcTest/ts/funcSpec.ts
+++ subprojects/com.toedter.chatty.client.web/src/funcTest/ts/funcSpec.ts
@@ -3,14 +3,14 @@
  * All rights reserved.
  * Licensed under MIT License, see http://toedter.mit-license.org/
  */
+/// <reference path="../../../typings/tsd.d.ts" />
+/// <reference path='../../typings/atmosphere.d.ts' />
+/// <reference path='../../../typings/jasmine/jasmine.d.ts' />
 
-/// <reference path='../../typescript-defs/jasmine.d.ts' />
-/// <reference path='../../typescript-defs/atmosphere.d.ts' />
-/// <reference path='../../typescript-defs/angular-protractor/angular-protractor.d.ts' />
 
 describe('Functional tests', () => {
 
-    it('should not allow connect without user data)', () => {
+    it('should not allow connect without user data', () => {
         "use strict";
         browser.get('http://localhost:8080/src/main/webapp/chatty.html');
         element(by.id('connectButton')).click();
@@ -18,10 +18,10 @@ describe('Functional tests', () => {
         expect(status.getText()).toContain('fill in');
     });
 
-    it('should not allow connect without user data)', () => {
+    it('should connect with valid user data', () => {
         "use strict";
         browser.get('http://localhost:8080/src/main/webapp/chatty.html');
-        element(by.model('userId')).sendKeys('toedter_k3');
+        element(by.model('userId')).sendKeys('toedter_k');
         element(by.model('userEmail')).sendKeys('kai@toedter.com');
         element(by.model('userFullName')).sendKeys('Kai Tödter');
         element(by.id('connectButton')).click();
