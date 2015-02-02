9400de59e0109af810dc789fda720ed93b8f2727
Kai Toedter <kai@toedter.com>
2015-02-02 17:18:23 +0100

    included timestamp in build info

--- subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/rest/BuildInfo.java
+++ subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/rest/BuildInfo.java
@@ -7,13 +7,19 @@ import org.springframework.hateoas.ResourceSupport;
 public class BuildInfo extends ResourceSupport {
 
     private final String version;
+    private final String timeStamp;
 
     @JsonCreator
-    public BuildInfo(@JsonProperty("version") String version) {
+    public BuildInfo(@JsonProperty("version") String version, @JsonProperty("timestamp") String timestamp) {
         this.version = version;
+        this.timeStamp = timestamp;
     }
 
     public String getVersion() {
         return version;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
     }
 }
--- subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/rest/BuildInfoController.java
+++ subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/rest/BuildInfoController.java
@@ -1,5 +1,7 @@
 package com.toedter.chatty.server.boot.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -8,16 +10,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Controller
 public class BuildInfoController {
+    private static Logger logger = LoggerFactory.getLogger(BuildInfoController.class);
+    
     @RequestMapping("/chatty/api/buildinfo")
     @ResponseBody
     public HttpEntity<BuildInfo> buildInfo() {
+        String version = "?";
+        String timeStamp = "?";
 
-        BuildInfo buildInfo = new BuildInfo("1.0.0");
+        Properties prop = new Properties();
+
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream stream = loader.getResourceAsStream("/build-info.properties");
+            if(stream != null) {
+                prop.load(stream);
+
+                version = prop.getProperty("version", "??");
+                timeStamp = prop.getProperty("timestamp", "??");
+            }
+
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+
+        BuildInfo buildInfo = new BuildInfo(version, timeStamp);
         buildInfo.add(linkTo(methodOn(BuildInfoController.class).buildInfo()).withSelfRel());
 
         return new ResponseEntity<>(buildInfo, HttpStatus.OK);
