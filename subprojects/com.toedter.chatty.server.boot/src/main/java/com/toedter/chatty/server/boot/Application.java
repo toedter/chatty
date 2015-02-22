666c5c9e5c859ceb09de76bbf0dc2d1315e7823d
Kai Toedter <kai@toedter.com>
2015-02-22 12:23:36 +0100

    now using @SpringBootApplication

--- subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/Application.java
+++ subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/Application.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -26,10 +27,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-// Todo: In next Spring Boot version use @SpringBootApplication
+@SpringBootApplication
 public class Application {
     private static Logger logger = LoggerFactory.getLogger(Application.class);
 
