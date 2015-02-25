87028b4d9a758563041c60c7bcf722ef0823cb79
Kai Toedter <kai@toedter.com>
2015-02-25 15:08:05 +0100

    build info now using PropertySource

--- subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/rest/BuildInfoController.java
+++ subprojects/com.toedter.chatty.server.boot/src/main/java/com/toedter/chatty/server/boot/rest/BuildInfoController.java
@@ -3,6 +3,7 @@ package com.toedter.chatty.server.boot.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,31 +18,18 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Controller
+@PropertySource({ "classpath:build-info.properties" })
 public class BuildInfoController {
     private static Logger logger = LoggerFactory.getLogger(BuildInfoController.class);
+
+    @Value( "${version}" )
+    private String version = "?";
+    @Value( "${timestamp}" )
+    private String timeStamp = "?";
     
     @RequestMapping("/chatty/api/buildinfo")
     @ResponseBody
     public HttpEntity<BuildInfo> buildInfo() {
-        String version = "?";
-        String timeStamp = "?";
-
-        Properties prop = new Properties();
-
-        try {
-            ClassLoader loader = Thread.currentThread().getContextClassLoader();
-            InputStream stream = loader.getResourceAsStream("/build-info.properties");
-            if(stream != null) {
-                prop.load(stream);
-
-                version = prop.getProperty("version", "??");
-                timeStamp = prop.getProperty("timestamp", "??");
-            }
-
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
-
         BuildInfo buildInfo = new BuildInfo(version, timeStamp);
         buildInfo.add(linkTo(methodOn(BuildInfoController.class).buildInfo()).withSelfRel());
 
