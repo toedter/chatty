21e487d887ef891ef8519a4a1a3416009dfc12d8
Kai Toedter <kai@toedter.com>
2016-03-26 13:07:28 +0100

    Angular2 beta 12

--- subprojects/com.toedter.chatty.client.angular2/src/app/ChattyComponent.ts
+++ subprojects/com.toedter.chatty.client.angular2/src/app/ChattyComponent.ts
@@ -1,3 +1,5 @@
+///<reference path="../../node_modules/angular2/typings/browser.d.ts"/>
+
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 import {ChatMessagesComponent} from './message/ChatMessagesComponent';
