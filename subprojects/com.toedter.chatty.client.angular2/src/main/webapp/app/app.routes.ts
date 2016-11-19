f6030ca1e026f8a5157c71466f5da0cceb003d03
Kai Toedter <kai@toedter.com>
2016-11-19 20:17:39 +0100

    default angular 2 route to messages

--- subprojects/com.toedter.chatty.client.angular2/src/main/webapp/app/app.routes.ts
+++ subprojects/com.toedter.chatty.client.angular2/src/main/webapp/app/app.routes.ts
@@ -5,7 +5,7 @@ import {ChatMessagesComponent} from './message/chatmessages.component';
 import {AboutComponent} from './about/about.component';
 
 export const routerConfig: Routes = [
-    { path: '', redirectTo: 'users', pathMatch: 'full' },
+    { path: '', redirectTo: 'messages', pathMatch: 'full' },
     { path: 'users', component: UsersComponent },
     { path: 'messages', component: ChatMessagesComponent },
     { path: 'about', component: AboutComponent }
