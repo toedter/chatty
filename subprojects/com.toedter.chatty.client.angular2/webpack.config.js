d4340587170f06ca269e219dc49efa03beaabe9c
Kai Toedter <kai@toedter.com>
2017-03-26 13:17:37 +0200

    updated to Angular 4

--- subprojects/com.toedter.chatty.client.angular2/webpack.config.js
+++ subprojects/com.toedter.chatty.client.angular2/webpack.config.js
@@ -18,7 +18,7 @@ var webpackConfig = {
     plugins: [
         new webpack.ContextReplacementPlugin(
             // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
+            /angular(\\|\/)core(\\|\/)@angular/,
             path.resolve(__dirname, './src'),
             {
                 // your Angular Async Route paths relative to this root directory
@@ -88,78 +88,3 @@ var defaultConfig = {
 
 
 module.exports = webpackMerge(defaultConfig, webpackConfig);
-/*
-var webpack = require('webpack');
-var path = require('path');
-
-
-// Webpack Config
-var webpackConfig = {
-    entry: {
-        'polyfills': './src/main/webapp/polyfills.browser.ts',
-        'vendor': './src/main/webapp/vendor.browser.ts',
-        'main': './src/main/webapp/main.browser.ts',
-    },
-
-    output: {
-        path: './dist',
-    },
-
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(true),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['main', 'vendor', 'polyfills'],
-            minChunks: Infinity
-        }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        })
-    ],
-
-    module: {
-        loaders: [
-            // .ts files for TypeScript
-            {test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader']},
-            {test: /\.css$/, loaders: ['to-string-loader', 'css-loader']},
-            {test: /\.html$/, loader: 'raw-loader'}
-        ]
-    }
-
-};
-
-
-// Our Webpack Defaults
-var defaultConfig = {
-    devtool: 'cheap-module-source-map',
-    cache: true,
-    debug: true,
-    output: {
-        filename: '[name].bundle.js',
-        sourceMapFilename: '[name].map',
-        chunkFilename: '[id].chunk.js'
-    },
-
-    resolve: {
-        root: [path.join(__dirname, 'src')],
-        extensions: ['', '.ts', '.js']
-    },
-
-    devServer: {
-        historyApiFallback: true,
-        watchOptions: {aggregateTimeout: 300, poll: 1000}
-    },
-
-    node: {
-        global: 1,
-        crypto: 'empty',
-        module: 0,
-        Buffer: 0,
-        clearImmediate: 0,
-        setImmediate: 0
-    }
-};
-
-var webpackMerge = require('webpack-merge');
-module.exports = webpackMerge(defaultConfig, webpackConfig);
-*/
