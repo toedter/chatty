3a2994ea31ea1ae5fe7dea4a6ea3bdb2591226ca
Kai Tödter <kai.toedter@siemens.com>
2017-11-10 18:55:30 +0100

    updated dependencies

--- subprojects/com.toedter.chatty.client.angular2/webpack.config.js
+++ subprojects/com.toedter.chatty.client.angular2/webpack.config.js
@@ -18,7 +18,7 @@ var webpackConfig = {
     plugins: [
         new webpack.ContextReplacementPlugin(
             // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\\|\/)core(\\|\/)@angular/,
+            /(.+)?angular(\\|\/)core(.+)?/,
             path.resolve(__dirname, './src'),
             {
                 // your Angular Async Route paths relative to this root directory
