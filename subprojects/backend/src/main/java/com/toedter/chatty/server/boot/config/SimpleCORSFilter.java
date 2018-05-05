53b07b8347f61374065c39f781622e2d4d11b551
Kai Toedter <kai@toedter.com>
2018-05-05 09:49:26 +0200

    fix(CORS): Allow-Methods = *

--- subprojects/backend/src/main/java/com/toedter/chatty/server/boot/config/SimpleCORSFilter.java
+++ subprojects/backend/src/main/java/com/toedter/chatty/server/boot/config/SimpleCORSFilter.java
@@ -12,7 +12,7 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Expose-Headers", "Location,Date,ETag,X-Application-Context");
