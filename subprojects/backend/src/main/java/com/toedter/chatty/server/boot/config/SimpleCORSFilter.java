97041fc98ec0cd6ecd4e5fffc5924360b385ab60
Kai Toedter <kai@toedter.com>
2018-05-05 10:21:36 +0200

    fix(CORS): added 'Link' to the CORS exposed headers

--- subprojects/backend/src/main/java/com/toedter/chatty/server/boot/config/SimpleCORSFilter.java
+++ subprojects/backend/src/main/java/com/toedter/chatty/server/boot/config/SimpleCORSFilter.java
@@ -15,7 +15,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "*");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Expose-Headers", "Location,Date,ETag,X-Application-Context");
+        response.setHeader("Access-Control-Expose-Headers", "Location,Date,Link,ETag,X-Application-Context");
         chain.doFilter(req, res);
 	}
 
