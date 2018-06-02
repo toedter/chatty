eafdaa0f56167baf48dc9cc5f08dfc3f9d01e1bd
Kai Toedter <kai@toedter.com>
2018-06-02 13:21:34 +0200

    fix: added Heroku container release script in Travis-CI build

---
+++ heroku-container-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+imageId=$(docker inspect registry.heroku.com/chatty42/web --format={{.Id}})
+payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
+curl -n -X PATCH https://api.heroku.com/apps/chatty42/formation \
+-d "$payload" \
+-H "Content-Type: application/json" \
+-H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+-H "Authorization: Bearer $HEROKU_AUTH_TOKEN"
