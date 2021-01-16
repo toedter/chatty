29a9b70186deccc6a342f544215fedc008a6620b
Kai Toedter <kai@toedter.com>
2021-01-16 10:50:46 +0100

    chore: update to Spring Boot 2.4.1

--- subprojects/backend/src/main/java/com/toedter/chatty/server/boot/user/web/UserConfiguration.java
+++ subprojects/backend/src/main/java/com/toedter/chatty/server/boot/user/web/UserConfiguration.java
@@ -3,12 +3,11 @@ package com.toedter.chatty.server.boot.user.web;
 import com.toedter.chatty.server.boot.user.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @Configuration
-public class UserConfiguration extends RepositoryRestConfigurerAdapter {
- 
+public class UserConfiguration implements RepositoryRestConfigurer {
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(User.class);
