6a539954736f7d599f36ef0565b13d3a42d5bca7
Kai Toedter <kai@toedter.com>
2021-11-14 21:06:29 +0100

    feat: expose 'Allow' header in CORS configuration

--- subprojects/backend/src/main/java/com/toedter/chatty/server/boot/user/web/UserConfiguration.java
+++ subprojects/backend/src/main/java/com/toedter/chatty/server/boot/user/web/UserConfiguration.java
@@ -4,12 +4,13 @@ import com.toedter.chatty.server.boot.user.User;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class UserConfiguration implements RepositoryRestConfigurer {
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.exposeIdsFor(User.class);
     }
 }
